ba7e8def39da8950e08af666d3f5d09b95b1cc1d
Prerak Mann <mannprerak2@gmail.com>
2023-03-04 17:41:34 +0530

    run formatter

--- lib/src/defs.dart
+++ lib/src/defs.dart
@@ -8,8 +8,11 @@ import 'package:nearby_connections/src/classes.dart';
 ///
 /// **P2P_POINT_TO_POINT** - single connection, very high bandwidth
 enum Strategy { P2P_CLUSTER, P2P_STAR, P2P_POINT_TO_POINT }
+
 enum Status { CONNECTED, REJECTED, ERROR }
+
 enum PayloadStatus { NONE, SUCCESS, FAILURE, IN_PROGRESS, CANCELED }
+
 enum PayloadType { NONE, BYTES, FILE, STREAM }
 
 //
