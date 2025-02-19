d00423772bc4aa88e0fb0b50067251918f11a6cf
Rahul Maindargi <rahulmaindargi@gmail.com>
2025-02-19 21:32:29 +0530

    Refactor: Remove Legacy Plugin Registration and Unused Code in NearbyConnectionsPlugin
    - Removed the legacy plugin registration method `registerWith` as it is no longer needed.
    - Removed unused `pluginRegistrar` variable.
    - Eliminated commented-out code related to method invocation within event callbacks for better readability.
    - Replaced the `//channel.invokeMethod` with `//` for readability in `onConnectionInitiated`, `onConnectionResult`, `onDisconnected`, `onPayloadReceived`, `onPayloadTransferUpdate`, `onEndpointFound`, and `onEndpointLost` callbacks.

--- android/src/main/java/com/pkmnapps/nearby_connections/NearbyConnectionsPlugin.java
+++ android/src/main/java/com/pkmnapps/nearby_connections/NearbyConnectionsPlugin.java
@@ -40,8 +40,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.EventChannel;
 
 /**
@@ -52,35 +50,12 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 	private static final String SERVICE_ID = "com.pkmnapps.nearby_connections";
 	private static MethodChannel channel;
 	private static EventChannel eventChannel;
-	private static PluginRegistry.Registrar pluginRegistrar;
 	private static EventChannel.EventSink eventSink;
 	private NearbyConnectionsPlugin(Activity activity) {
 		this.activity = activity;
 	}
 
 	public NearbyConnectionsPlugin() {
-	}
-
-	/**
-	 * Legacy Plugin registration.
-	 */
-
-	public static void registerWith(Registrar registrar) {
-		pluginRegistrar = registrar;
-		channel = new MethodChannel(registrar.messenger(), "nearby_connections");
-		channel.setMethodCallHandler(new NearbyConnectionsPlugin(registrar.activity()));
-//		eventChannel=new EventChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), "nearby_connections/events");
-//		eventChannel.setStreamHandler(
-//				new EventChannel.StreamHandler() {
-//					@Override
-//					public void onListen(Object arguments, EventChannel.EventSink events) {
-//						eventSink=events;
-//					}
-//					@Override
-//					public void onCancel(Object arguments) {
-//						eventSink=null;
-//					}
-//				});
 	}
 
 	@Override
@@ -298,7 +273,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			args.put("endpointName", connectionInfo.getEndpointName());
 			args.put("authenticationToken", connectionInfo.getAuthenticationToken());
 			args.put("isIncomingConnection", connectionInfo.isIncomingConnection());
-//			channel.invokeMethod("ad.onConnectionInitiated", args);
+
 			args.put("method", "ad.onConnectionInitiated");
 			eventSink.success(args);
 		}
@@ -326,7 +301,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 					// Unknown status code
 			}
 			args.put("statusCode", statusCode);
-			//channel.invokeMethod("ad.onConnectionResult", args);
+
 			args.put("method", "ad.onConnectionResult");
 			eventSink.success(args);
 		}
@@ -336,7 +311,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			Log.d("nearby_connections", "ad.onDisconnected");
 			Map<String, Object> args = new HashMap<>();
 			args.put("endpointId", endpointId);
-			//channel.invokeMethod("ad.onDisconnected", args);
+
 			args.put("method", "ad.onDisconnected");
 			eventSink.success(args);
 		}
@@ -351,7 +326,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			args.put("endpointName", connectionInfo.getEndpointName());
 			args.put("authenticationToken", connectionInfo.getAuthenticationToken());
 			args.put("isIncomingConnection", connectionInfo.isIncomingConnection());
-			//channel.invokeMethod("dis.onConnectionInitiated", args);
+
 			args.put("method", "dis.onConnectionInitiated");
 			eventSink.success(args);
 		}
@@ -379,7 +354,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 					// Unknown status code
 			}
 			args.put("statusCode", statusCode);
-		//	channel.invokeMethod("dis.onConnectionResult", args);
+
 			args.put("method", "dis.onConnectionResult");
 			eventSink.success(args);
 		}
@@ -389,7 +364,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			Log.d("nearby_connections", "dis.onDisconnected");
 			Map<String, Object> args = new HashMap<>();
 			args.put("endpointId", endpointId);
-			//channel.invokeMethod("dis.onDisconnected", args);
+
 			args.put("method","dis.onDisconnected");
 			eventSink.success(args);
 		}
@@ -416,7 +391,6 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 				}
 			}
 
-			//channel.invokeMethod("onPayloadReceived", args);
 			args.put("method","onPayloadReceived");
 			eventSink.success(args);
 		}
@@ -434,7 +408,6 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			args.put("bytesTransferred", payloadTransferUpdate.getBytesTransferred());
 			args.put("totalBytes", payloadTransferUpdate.getTotalBytes());
 
-			//channel.invokeMethod("onPayloadTransferUpdate", args);
 			args.put("method","onPayloadTransferUpdate");
 			eventSink.success(args);
 		}
@@ -449,7 +422,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			args.put("endpointId", endpointId);
 			args.put("endpointName", discoveredEndpointInfo.getEndpointName());
 			args.put("serviceId", discoveredEndpointInfo.getServiceId());
-			//channel.invokeMethod("dis.onEndpointFound", args);
+
 			args.put("method","dis.onEndpointFound");
 			eventSink.success(args);
 		}
@@ -459,7 +432,7 @@ public class NearbyConnectionsPlugin implements MethodCallHandler, FlutterPlugin
 			Log.d("nearby_connections", "onEndpointLost");
 			Map<String, Object> args = new HashMap<>();
 			args.put("endpointId", endpointId);
-			//channel.invokeMethod("dis.onEndpointLost", args);
+
 			args.put("method","dis.onEndpointLost");
 			eventSink.success(args);
 		}
